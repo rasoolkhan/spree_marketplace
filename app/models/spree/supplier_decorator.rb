f63c424801c0308ee8be6cb39b32531e24fdf5f0
Denis Gukov <denguk@gmail.com>
2015-10-25 21:55:02 +0500

    Fixed bug with image sizes

--- app/models/spree/supplier_decorator.rb
+++ app/models/spree/supplier_decorator.rb
@@ -1,14 +1,18 @@
 Spree::Supplier.class_eval do
   has_attached_file :background,
-    styles: { large: '1200x300>' },
+    styles: { large: '1200x300#' },
     default_style: :large,
     url: '/spree/suppliers/background/:id/:style/:basename.:extension',
     path: ':rails_root/public/spree/suppliers/background/:id/:style/:basename.:extension',
     default_url: 'default_supplier_background.jpg'
   has_attached_file :avatar,
-    styles: { small: '50x50', product:'300x300', large: '800x600>' },
+    styles: { small: '50x50#', product:'300x300#', large: '800x600>' },
     default_style: :product,
     url: '/spree/suppliers/avatar/:id/:style/:basename.:extension',
     path: ':rails_root/public/spree/suppliers/avatar/:id/:style/:basename.:extension',
     default_url: 'noimage/product.png'
+
+  validates_attachment :avatar, content_type: { content_type: /\Aimage\/.*\Z/ }
+  validates_attachment :background, content_type: { content_type: /\Aimage\/.*\Z/ }
+
 end
--- app/overrides/add_fields_to_admin_panel.rb
+++ app/overrides/add_fields_to_admin_panel.rb
@@ -7,6 +7,7 @@ Deface::Override.new(virtual_path: 'spree/admin/suppliers/_form',
       <%= form.text_field :slogan, :class => 'form-control' %>
     <% end %>"
 )
+
 Deface::Override.new(virtual_path: 'spree/admin/suppliers/_form',
   name: 'add_description_to_admin_panel',
   insert_before: 'erb:contains("spree_current_user.admin?")',
@@ -14,5 +15,25 @@ Deface::Override.new(virtual_path: 'spree/admin/suppliers/_form',
     "<%= form.field_container :description, class: ['form-group'] do %>
       <%= form.label :description, Spree::Supplier.human_attribute_name(:description) %>:<br />
       <%= form.text_area :description, :class => 'form-control' %>
+    <% end %>"
+)
+
+Deface::Override.new(virtual_path: 'spree/admin/suppliers/_form',
+  name: 'add_background_to_admin_panel',
+  insert_before: 'erb:contains("spree_current_user.admin?")',
+  text:
+    "<%= form.field_container :background, class: ['form-group'] do %>
+      <%= form.label :background, Spree.t(:background) %>
+      <%= form.file_field :background %>
+    <% end %>"
+)
+
+Deface::Override.new(virtual_path: 'spree/admin/suppliers/_form',
+  name: 'add_avatar_to_admin_panel',
+  insert_before: 'erb:contains("spree_current_user.admin?")',
+  text:
+    "<%= form.field_container :avatar, class: ['form-group'] do %>
+      <%= form.label :avatar, Spree.t(:avatar) %>
+      <%= form.file_field :avatar %>
     <% end %>"
 )
