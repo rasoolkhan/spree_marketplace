fbe4391152615e24fc4d73e6af8292dcd62f5631
Denis Gukov <denguk@gmail.com>
2015-11-07 16:11:14 +0500

    Hide shipping category combobox if only one shipping category exists

---
+++ app/overrides/add_js_for_select_default_shipping_cat_of_product.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(virtual_path: 'spree/admin/products/new',
+  name: 'add_js_for_select_default_shipping_cat_of_product',
+  insert_after: 'script',
+  text: "<script>
+    $(function() {
+      // Hide shipping category combobox if only one shipping category exists
+      var $select = $('select[name=\"product[shipping_category_id]\"]');
+      if ($select.find('option').size() <= 2) {
+        $select.val(1);
+        $('div[data-hook=\"new_product_shipping_category\"]').hide();
+      }
+    });
+  </script>"
+)
