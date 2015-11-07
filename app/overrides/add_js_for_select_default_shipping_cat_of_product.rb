6a0ea26d0eda60561ea73bcd47124ad7dd19b276
Denis Gukov <denguk@gmail.com>
2015-11-07 16:12:47 +0500

    Changed condition of hidding shipping category combobox

--- app/overrides/add_js_for_select_default_shipping_cat_of_product.rb
+++ app/overrides/add_js_for_select_default_shipping_cat_of_product.rb
@@ -5,7 +5,7 @@ Deface::Override.new(virtual_path: 'spree/admin/products/new',
     $(function() {
       // Hide shipping category combobox if only one shipping category exists
       var $select = $('select[name=\"product[shipping_category_id]\"]');
-      if ($select.find('option').size() <= 2) {
+      if ($select.find('option').size() == 2) {
         $select.val(1);
         $('div[data-hook=\"new_product_shipping_category\"]').hide();
       }
